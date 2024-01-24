94ab01d535fa51af8713c263c0e57135aebf5f45
dinky <132214285+dinkyjani27@users.noreply.github.com>
2024-01-24 09:14:40 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,17 @@
+"use strict";  // treat all JS code as newer version
+// alert ( 3 + 3)  we are using node js not browser
+
+//number =>  2 to power 53
+//bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value // null is an object
+// undefined =>
+// symbol => unique
+
+
+//object
+let name="dinky"
+console.log(typeof "dinky")
+console.log(typeof undefined) // output = undefined
+console.log(typeof null);  // output = object
