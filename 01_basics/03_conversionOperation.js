a035c47391a8a4048cbb8dccd3180891f103b899
dinky <132214285+dinkyjani27@users.noreply.github.com>
2024-01-30 07:02:27 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,33 +1,46 @@
-let score = "Dinky"
-console.log(typeof score)
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+// let score = "Dinky"
+// console.log(typeof score)
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
 
-// "33" => 33
-// "33abc" => NaN
-// true = 1 ; false = 0;
-
-
-let isLoggedIn = 1
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// // "33" => 33
+// // "33abc" => NaN
+// // true = 1 ; false = 0;
 
 
-let isLoggedIns = ""
-let booleanIsLoggedIns = Boolean(isLoggedIns)
-console.log(booleanIsLoggedIns);
-
-let isLoggedInss = "dinky"
-let booleanIsLoggedInss = Boolean(isLoggedInss)
-console.log(booleanIsLoggedInss);
-
-// 1 => true; 0 => falsee
-// "" => false
-// "dinky" => true
+// let isLoggedIn = 1
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
 
 
-let someNUmber = 22
-let stringNumber = String(someNUmber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let isLoggedIns = ""
+// let booleanIsLoggedIns = Boolean(isLoggedIns)
+// console.log(booleanIsLoggedIns);
+
+// let isLoggedInss = "dinky"
+// let booleanIsLoggedInss = Boolean(isLoggedInss)
+// console.log(booleanIsLoggedInss);
+
+// // 1 => true; 0 => falsee
+// // "" => false
+// // "dinky" => true
+
+
+// let someNUmber = 22
+// let stringNumber = String(someNUmber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// *****************Operations **************
+let value = 3
+let negValue = -value;
+console.log(negValue);
+
+console.log(2+2);
+console.log(1+"2")
+console.log("1"+3)
+console.log("1"+"2")
+console.log("1"+2+2)
+console.log(1+2+"2")
