d27f9a5089d218cac2908570609c9431bb39a569
dinky <132214285+dinkyjani27@users.noreply.github.com>
2024-01-30 14:33:54 +0000

    date

---
+++ 01_basics/07_datesin.js
@@ -0,0 +1,19 @@
+//dates
+
+let mydate = new Date()
+// console.log(mydate);
+// console.log(mydate.toString());
+// console.log(mydate.toLocaleString());
+// console.log(mydate.toDateString());
+// console.log(typeof mydate);
+
+let mycreatedDate = new Date(2023, 0,23,5,3)
+// console.log(mycreatedDate.toLocaleString());
+
+
+let mysecocreatedDate = new Date("2023-01-14")
+
+// console.log(mysecocreatedDate.toLocaleString())
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp);
