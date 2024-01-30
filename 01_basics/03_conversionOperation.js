ff7539acdb3e1ce301750fa527a4dc8fd2f45239
dinky <132214285+dinkyjani27@users.noreply.github.com>
2024-01-30 06:35:43 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,33 @@
+let score = "Dinky"
+console.log(typeof score)
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+// "33" => 33
+// "33abc" => NaN
+// true = 1 ; false = 0;
+
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+let isLoggedIns = ""
+let booleanIsLoggedIns = Boolean(isLoggedIns)
+console.log(booleanIsLoggedIns);
+
+let isLoggedInss = "dinky"
+let booleanIsLoggedInss = Boolean(isLoggedInss)
+console.log(booleanIsLoggedInss);
+
+// 1 => true; 0 => falsee
+// "" => false
+// "dinky" => true
+
+
+let someNUmber = 22
+let stringNumber = String(someNUmber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
