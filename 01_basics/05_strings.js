3f61020db88bbeb465f021aa3ac2c896584ca5e6
dinky <132214285+dinkyjani27@users.noreply.github.com>
2024-01-30 14:01:42 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,10 @@
+const name = "dinky"
+const reppoCount = 50
+//console.log(name + reppoCount + "Value")
+console.log(` Hell0 my name is ${name} and my repocount is ${reppoCount}`)
+
+const gamename = new String("dinkyjani")
+console.log(gamename.__proto__);
+console.log(gamename[0]);
+console.log(gamename.toUpperCase());
+console.log(gamename.length);
